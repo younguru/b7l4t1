27cf1b7dc53730f2785b0b7dd9c15b7ebdd04722
younguru <kiosun@mail.ru>
2022-04-04 23:30:59 +0300

    set id fix, non thread safe long fix

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -4,11 +4,12 @@ import ru.netology.model.Post;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepository {
     private Map<Long, Post> postBase = new ConcurrentHashMap<>();
-    private long counter = 0;
+    private AtomicLong counter = new AtomicLong(0);
 
     public List<Post> all() {
         return new ArrayList<>(postBase.values());
@@ -18,16 +19,14 @@ public class PostRepository {
         return Optional.ofNullable(postBase.get(id));
     }
 
-    public Post save(Post post) {
-        long id = post.getId();
-        if (id == 0) {
-            id = counter;
-            counter += 1;
+    synchronized public Post save(Post post) {
+        if (post.getId() == 0) {
+            post.setId(counter.getAndIncrement());
         }
-        return postBase.put(id, post);
+        return postBase.put(post.getId(), post);
     }
 
-    public void removeById(long id) {
+    synchronized public void removeById(long id) {
         postBase.remove(id);
     }
 }
